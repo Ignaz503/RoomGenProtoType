f4cf0f3e5bef609877bd5e2131475f5f6084d3d4
Ignaz\Lukas <lukasneuhold2@gmail.com>
2018-03-23 18:18:34 +0100

    Trigger Tests

---
+++ RoomGenPrototype/Assets/Scripts/Display/MetadataDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetadataDisplay : MonoBehaviour {
+
+    public static MetadataDisplay Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void SetActiveTo(bool value)
+    {
+        Debug.Log(value);
+        gameObject.SetActive(value);
+    }
+}
---
+++ RoomGenPrototype/Assets/Scripts/Display/MetadataDisplayTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MetadataDisplayTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MetadataDisplay.Instance.SetActiveTo(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        MetadataDisplay.Instance.SetActiveTo(false);
+    }
+}
