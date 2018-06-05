0c5e06c66a3a7cca64226226e4b2f03bd58d2658
Ignaz\Lukas <lukasneuhold2@gmail.com>
2018-06-05 17:13:15 +0200

    fixed typo in var name

--- RoomGenPrototype/Assets/Scripts/Player/Player.cs
+++ RoomGenPrototype/Assets/Scripts/Player/Player.cs
@@ -33,8 +33,8 @@ public class Player : MonoBehaviour {
 
     public Camera PlayerCam;
     Ray centerRay;
-    [SerializeField][Range(0.5f, 100f)] float rayCasMaxDist;
-    public float RayCastMaxDist { get { return rayCasMaxDist; } protected set { rayCasMaxDist = value; } }
+    [SerializeField][Range(0.5f, 100f)] float rayCastMaxDist;
+    public float RayCastMaxDist { get { return rayCastMaxDist; } protected set { rayCastMaxDist = value; } }
     [SerializeField] KeyCode interactKey;
     [SerializeField] InteractionPrompt interactionPrompt;
     public FirstPersonController FirstPersonController;
